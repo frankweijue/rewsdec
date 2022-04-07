dae9c735b1d8ddc8584650fa2c4b0887359d4227
frankweijue <769032193@qq.com>
2022-04-07 10:57:43 +0000

    v1

---
+++ test.py
@@ -0,0 +1,4 @@
+import torch.nn as nn
+class testnet(nn.Module):
+    def __init__(self):
+        super(testnet, self).__init__()
---
+++ third_party.sh
@@ -0,0 +1,3 @@
+mkdir third_party
+cd third_party
+git clone https://github.com/ranjaykrishna/densevid_eval.git
